c33ceda7a6ad65cc8f300352c55669f209fded7f
Dgau-Glitch <dgau5@mail.ru>
2026-04-09 15:50:50 +0700

    first commit

---
+++ src/main/kotlin/ru/fatumsoft/searchItems/SearchItems.kt
@@ -0,0 +1,14 @@
+package ru.fatumsoft.searchItems
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SearchItems : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
